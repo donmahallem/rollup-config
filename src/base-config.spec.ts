f617dc2292ef83b6aed6bd95a9e9f4a7ca689db8
donmahallem <donmahallem@users.noreply.github.com>
2023-12-22 12:17:13 +0100

    test(c8): replace nyc with c8 for coverage summary

--- src/base-config.spec.ts
+++ src/base-config.spec.ts
@@ -8,7 +8,7 @@ import 'mocha';
 import { RollupOptions } from 'rollup';
 import { baseConfig } from './base-config.js';
 
-describe('index', (): void => {
+describe('base-config.spec.ts', (): void => {
     it('should set all plugins with default config', (): void => {
         const result: RollupOptions = baseConfig({ name: 'testname', version: '1.2.3' });
         expect(result.plugins).to.have.lengthOf(5);
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -8,7 +8,7 @@ import 'mocha';
 import { RollupOptions } from 'rollup';
 import defaultConfig from './index.js';
 
-describe('index', (): void => {
+describe('index.ts', (): void => {
     it('should set all plugins with default config', (): void => {
         const result: RollupOptions = defaultConfig({ name: 'testname', version: '1.2.3' });
         expect(result.plugins).to.have.lengthOf(5);
