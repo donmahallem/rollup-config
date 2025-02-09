62cea6e039514305c18f399a92c25db02ff93c59
donmahallem <donmahallem@users.noreply.github.com>
2025-02-09 17:04:52 +0100

    style: fix style issues

--- src/base-config.spec.ts
+++ src/base-config.spec.ts
@@ -8,16 +8,18 @@ import 'mocha';
 import { RollupOptions } from 'rollup';
 import { baseConfig } from './base-config.js';
 
-describe('base-config.spec.ts', (): void => {
-    it('should set all plugins with default config', (): void => {
+describe('base-config.spec.ts', function (): void {
+    it('should set all plugins with default config 1', function (): void {
         const result: RollupOptions = baseConfig({ name: 'testname', version: '1.2.3' });
         expect(result.plugins).to.have.lengthOf(5);
     });
-    it('should exclude commonjs with false as config', (): void => {
+
+    it('should exclude commonjs with false as config 2', function (): void {
         const result: RollupOptions = baseConfig({ name: 'testname', version: '1.2.3' }, { plugins: { commonjs: false } });
         expect(result.plugins).to.have.lengthOf(4);
     });
-    it('should exclude commonjs with false as config', (): void => {
+
+    it('should exclude commonjs with false as config 3', function (): void {
         const result: RollupOptions = baseConfig({ name: 'testname', version: '1.2.3' }, { plugins: { json: false } });
         expect(result.plugins).to.have.lengthOf(4);
     });
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -8,8 +8,8 @@ import 'mocha';
 import { RollupOptions } from 'rollup';
 import defaultConfig from './index.js';
 
-describe('index.ts', (): void => {
-    it('should set all plugins with default config', (): void => {
+describe('index.ts', function (): void {
+    it('should set all plugins with default config', function (): void {
         const result: RollupOptions = defaultConfig({ name: 'testname', version: '1.2.3' });
         expect(result.plugins).to.have.lengthOf(5);
     });
